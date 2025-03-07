ad3627f3efe0619c3dff37b50f80c761a8f28d1f
Dominika Šenová <domisenova@seznam.cz>
2025-03-07 14:28:36 +0100

    descriptors, basic clustering

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import Descriptors
+import peptides
 
 
 '''
@@ -11,24 +10,16 @@ Create a dataframe with molecular descriptors for a list of peptides
 # Read the positive peptides from the CSV file to dataframe
 positive_df = pd.read_csv("../data/examples_DBAASP/positive_peptides_without_modifications.csv")
 
+# Create a DataFrame of lists with the descriptors for each peptide
+descriptors_list_list = []
+for i in range(positive_df.shape[0]):
+    peptide = peptides.Peptide(positive_df.iloc[i,0])
+    descriptors_list_list.append(peptide.pcp_descriptors())
+descriptors_positives_df = pd.DataFrame(descriptors_list_list)
 
-# Convert the AA sequences to RDKit molecules
-#mols_positives = [Chem.MolFromSequence(peptide) for peptide in positive_df['SEQUENCE']]
-
-
-# Calculate the molecular descriptors
-# # TODO: use MorganGenerator instead
-# descriptors_positives_list = []
-# for mol in range(len(mols_positives)):
-#     mol_descriptors = Descriptors.CalcMolDescriptors(mol)
-#     descriptors_positives_list.append(mol_descriptors)
-
-
-# Create a pandas DataFrame from the list of peptide descriptors
-#descriptors_positives_df = pd.DataFrame.from_dict(descriptors_positives_list)
 
 # Save the DataFrames to a CSV file
-#descriptors_positives_df.to_csv('../data/examples_DBAASP/descriptors_positive_staphylococcus.csv', index=False)
+descriptors_positives_df.to_csv('../data/examples_DBAASP/descriptors_positive_staphylococcus.csv', index=False)
 
 
 
