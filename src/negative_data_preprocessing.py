faed6150768aa86e095f455b012714d628890983
Dominika Šenová <domisenova@seznam.cz>
2025-03-11 16:00:49 +0100

    comparison with negative peptides

---
+++ src/negative_data_preprocessing.py
@@ -0,0 +1,16 @@
+import peptides
+import pandas as pd
+
+with open("../data/examples_random_peptides_SwissProt/negative_peptides") as f:
+    # readlines without \n
+    negative_peptides = f.read().splitlines()
+print(negative_peptides[:5])
+
+decsriptors_list_list = []
+for i in range(len(negative_peptides)):
+    print(negative_peptides[i])
+    peptide = peptides.Peptide(negative_peptides[i])
+    decsriptors_list_list.append(peptide.descriptors())
+descriptors_negatives_df = pd.DataFrame(decsriptors_list_list)
+
+descriptors_negatives_df.to_csv("../data/examples_random_peptides_SwissProt/descriptors_negative.csv", index=False)
