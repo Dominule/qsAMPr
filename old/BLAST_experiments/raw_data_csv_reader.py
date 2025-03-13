93ec3ab5faf940941f2ce8040eb37c37164ad3f9
Dominika Šenová <domisenova@seznam.cz>
2025-03-13 17:20:13 +0100

    visualize datasets

--- old/BLAST_experiments/raw_data_csv_reader.py
+++ old/BLAST_experiments/raw_data_csv_reader.py
@@ -7,7 +7,7 @@ Create a csv file with just the peptide sequences
 
 peptide_sequences = []
 peptide_MIC_values = []
-with open("../data/samples_DBAASP/raw_dbaasp_staphylococcus_without_modifications.csv") as file:
+with open("../../data/samples_DBAASP/raw_dbaasp_staphylococcus_without_modifications.csv") as file:
     reader = csv.reader(file, delimiter=",")
     for row in reader:
         peptide_sequences.append(row[4])
